02e0b2f807e04ba1780da89611879cdcf2ffa95c
sauravdhakal12 <sauravdhakal121@gmail.com>
2021-07-26 11:37:06 +0545

    FASTER FIRST MOVE comments added

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from random import randint
 
 X = "X"
 O = "O"
@@ -28,10 +29,12 @@ def player(board):
     x_count = 0
     o_count = 0
 
+    #Count no of X and no of O in the board
     for row in board:
         x_count += row.count(X)
         o_count += row.count(O)
     
+    #Find the turn
     if x_count > o_count:
         return O
     # elif o_count > x_count:
@@ -44,8 +47,10 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+
     action = []
 
+    #Find empty cells in the board
     for i,row in enumerate(board):
         for j,col in enumerate(row):
             if not board[i][j]:
@@ -61,6 +66,7 @@ def result(board, action):
     x,y = action
     turn = player(board)
 
+    #Fill on certain cell and return the resulting board
     for i,row in enumerate(board):
         tmp = []
         for j,col in enumerate(row):
@@ -80,6 +86,7 @@ def winner(board):
     """
     inverted_board = []
 
+    #Invert the board
     for i,row in enumerate(board):
         tmp = []
         for j,col in enumerate(row):
@@ -87,23 +94,27 @@ def winner(board):
         
         inverted_board.append(tmp)
 
+    #Check if three rows are same or not
     for row in board:
         if row.count("X") == 3:
             return X
         elif row.count("O") == 3:
             return O
     
+    #Agian for the inverted board
     for row in inverted_board:
         if row.count("X") == 3:
             return X
         elif row.count("O") == 3:
             return O
 
+    #Check diagonal
     if board[0][0] == board[1][1] == board[2][2]:
         return board[1][1]
     elif board[0][2] == board[1][1] == board[2][0]:
         return board[1][1]
 
+    #No-winner
     return None
 
 
@@ -114,6 +125,7 @@ def terminal(board):
     if winner(board):
         return True
     
+    #Check if any cell is empty
     for row in board:
         for col in row:
             if not col:
@@ -128,6 +140,7 @@ def utility(board):
     """
     won = winner(board)
 
+    #1,0,-1
     if won == X:
         return 1
     elif won == O:
@@ -142,45 +155,64 @@ def minimax(board):
     """
     if terminal(board):
         return None
+
+    #The first move
+    elif board == initial_state():
+        return [(0,0),(0,2),(1,1),(2,0),(2,2)][randint(0,4)]
     
+    #Whose turn
     turn = player(board)
 
+    #If AI is X
     if turn == X:
         moves =  max_value(board)
-
-        print(moves, 1)
-        # moves.sort(key=lambda a : a[1], reverse=True)
-        return moves[0 ]
-
-    else:
-        moves = min_value(board)
-        print(moves, 2)
-        # moves.sort(key=lambda a : a[1])
         return moves[0]
 
+    #If AI is Y
+    else:
+        moves = min_value(board)
+        return moves[0]
+
+
 def max_value(board):
+    """ 
+        IF AI is X, find it's optimal move.
+    """
     max_results = []
 
+    #If game-over, return utility of the board
     if terminal(board):
         return (1,utility(board))
 
+    #Find all possible moves(recursion)
     for action in actions(board):
+
+        #Get the value of the previous state
         value = min_value(result(board,action))
         max_results.append((action, value[1]))
 
+    #Return the best action
     max_results.sort(key=lambda a:a[1], reverse=True)    
     return max_results[0]
 
 
 def min_value(board):
+    """ 
+        IF AI is Y, find it's optimal move.
+    """
     min_results = []
     
+    #If game-over, return utility of the board
     if terminal(board):
         return (1,utility(board))
 
+    #Find all possible moves(recursion)
     for action in actions(board):
+
+        #Get the value of the previous state
         value = max_value(result(board,action))
         min_results.append((action,value[1]))
-        
+
+    #Return the best action
     min_results.sort(key=lambda a:a[1])
     return min_results[0]
